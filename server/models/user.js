32705f882928a7b7c30c777214d103b1296d8e3e
Jamie <Sinclair>
2021-02-08 19:15:18 +0800

    bug fix

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const userSchema = mongoosse.Schema({
+const userSchema = mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
